4ddecd9f632e01067f6e5890432253f2e4baf61f
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2014-04-23 22:47:27 +0900

    Create Project, Init Commit.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,61 @@
 #! /usr/bin/env python
 
-import asyncore
-import uuid
+import getpass, poplib
+import ConfigParser
+import re
+import importlib
 
-from smtpd import SMTPServer
+from email.parser import Parser
+from email.header import decode_header
 
-class MailServer(SMTPServer):
-    no = 0
-    def process_message(self, peer, mailfrom, rcpttos, data):
-        f = open('%s.eml' % uuid.uuid4(), 'w')
-        f.write(data)
-        f.close
+def main():
+    config = ConfigParser.RawConfigParser()
+    config.read('mail.cfg')
 
-def run():
-    foo = MailServer(('localhost', 25), None)
-    try:
-        asyncore.loop()
-    except KeyboardInterrupt:
-        pass
+    host = config.get('Host', 'POP3')
+    ssl = config.getint('Host', 'SSL')
+
+    user = config.get('Account', 'User')
+    password = config.get('Account', 'Password')
+
+    rules = []
+    for rule in config.options('Rules'):
+        rules.append({ 'module': rule, 'rule': config.get('Rules', rule) })
+
+    if user and password:
+        if ssl:
+            print('Connect to %s by SSL' % host)
+            mail = poplib.POP3_SSL(host, 995)
+        else:
+            print('Connect to %s' % host)
+            mail = poplib.POP3(host, 995)
+
+        mail.user(user)
+        mail.pass_(password)
+        total = len(mail.list()[1])
+
+        print('%d mail(s) fetched.' % total)
+
+        for i in range(total):
+            headers = Parser().parsestr('\n'.join(mail.retr(i+1)[1]))
+            subject = decode_header(headers['subject'])[0][0]
+
+            matched = False
+            
+            for r in rules:
+                m = re.match(r['rule'], subject, re.I)
+                if m: # find registered rule
+                    matched = True
+                    
+                    module = importlib.import_module('modules.' + r['module'])
+                    module.main(m.group('param'))
+
+            if matched:
+                print(matched)
+                mail.dele(i+1) # delete mail
+
+        mail.quit()
 
 if __name__ == '__main__':
-    run()
+    main()
+
---
+++ src/modules/echo.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+def main(param):
+    # implement your command
+    print('echo module', param)
+
+if __name__ == '__main__':
+    main()
+
